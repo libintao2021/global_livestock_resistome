ce59368357df4b5f17fb753deb660c302747bbe9
libintao2021 <94350762+libintao2021@users.noreply.github.com>
2024-09-20 16:13:28 +0800

    Create Figure5.R

---
+++ Code for Figures/Figure5.R
@@ -0,0 +1,21 @@
+# library
+library(pheatmap)
+library(tibble)
+library(dplyr)
+
+# create a dataset
+heatmap_matrix <- read.delim('heatmap_matrix.txt', row.names = 1, sep = '\t', stringsAsFactors = FALSE, check.names = FALSE,na.strings="na")
+
+# Plot
+pheatmap(t(heatmap_matrix),
+         clustering_distance_rows = "euclidean",  
+         clustering_distance_cols = "euclidean", 
+         clustering_method = "complete",          
+         cluster_cols = FALSE,                    
+         cluster_rows = TRUE,                   
+         scale = "row",                   
+         color = colorRampPalette(c("#1f78b4", "white", "#ff7f00"))(255), 
+         angle_col = 45,                        
+         border_color = NA,                   
+         fontsize = 12,                       
+         legend_title = "Prevalence")     
